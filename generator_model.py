58583bdf17752a59d8986add457afe51d2d45953
JoJo Feinstein <joseph_feinstein@brown.edu>
2021-04-12 23:23:35 -0400

    finished generator model i think

--- generator_model.py
+++ generator_model.py
@@ -1,6 +1,6 @@
 import numpy
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, LeakyReLU, Dropout, BatchNormalization, Reshape, UpSampling2D, Conv2D, Activation
+from tensorflow.keras.layers import Dense, LeakyReLU, Dropout, BatchNormalization, UpSampling2D, Conv2D, Activation
 
 
 class Generator(tf.keras.Model):
@@ -21,25 +21,40 @@ class Generator(tf.keras.Model):
         self.leakyrelu2 = LeakyReLU()
 
         self.upsample2 = UpSampling2D(size=(2, 2))
-        self.conv2 = Conv2D(filters=3, kernel_size=5, padding='same')
+        self.conv2 = Conv2D(filters=64, kernel_size=5, padding='same')
+        self.batchnorm2 = BatchNormalization()
+        self.leakyrelu3 = LeakyReLU()
+
+        self.upsample3 = UpSampling2D(size=(2, 2))
+        self.conv3 = Conv2D(filters=3, kernel_size=5, padding='same')
         self.tanh = Activation('tanh')
 
-    def call(self, x):
+    def call(self, encoded_image, label):
         """ Passes input image through the network. """
+
+        x = tf.concat([encoded_image, label], 1)
 
         x = self.dense1(x)
         x = self.leakyrelu1(x)
         x = self.dropout1(x)
 
-        x = self.reshape(x)
         x = self.upsample1(x)
         x = self.conv1(x)
         x = self.batchnorm1(x)
         x = self.leakyrelu2(x)
 
-        x = self.upsample1(x)
+        x = self.upsample2(x)
         x = self.conv2(x)
+        x = self.batchnorm2(x)
+        x = self.leakyrelu3(x)
+
+        x = self.upsample3(x)
+        x = self.conv3(x)
         x = self.tanh(x)
 
         return x
 
+    def loss_function(self, y_true, y_pred):
+        loss_fn = tf.keras.losses.BinaryCrossentropy()
+
+        return loss_fn(y_true, y_pred)
