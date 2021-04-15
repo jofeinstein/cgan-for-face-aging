93deeedef678a74fe2795a87bead2137177119b1
JoJo Feinstein <joseph_feinstein@brown.edu>
2021-04-15 11:10:32 -0400

    initial commit. used to download data and form data directory structure

---
+++ download_data.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+mkdir -p data
+cd data
+wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/imdb_crop.tar
+tar -xf imdb_crop.tar
+rm imdb_crop.tar
+mkdir -p training_logs
+mkdir -p weights
