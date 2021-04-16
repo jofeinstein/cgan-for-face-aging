3dda524805e0a07a5ae1e444266f57c002d16c16
JoJo Feinstein <joseph_feinstein@brown.edu>
2021-04-16 07:48:55 -0400

    commented out dense

--- discriminator_model.py
+++ discriminator_model.py
@@ -12,7 +12,7 @@ class Discriminator(tf.keras.Model):
         self.optimizer = tf.keras.optimizers.Adam()
 
         self.embedding = Embedding(input_dim=6, output_dim=1024)
-        self.dense_e = Dense(1024)
+        # self.dense_e = Dense(1024)
         self.reshape = Reshape((32, 32, 6))
 
         self.conv1 = Conv2D(64, kernel_size=3, strides=2, padding='same')
