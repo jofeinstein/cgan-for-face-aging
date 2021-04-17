425562370726779af6883f02880a92c84653481e
JoJo Feinstein <joseph_feinstein@brown.edu>
2021-04-16 22:23:26 -0400

    added echo

--- download_data.sh
+++ download_data.sh
@@ -3,6 +3,7 @@
 mkdir -p data
 cd data
 wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/imdb_crop.tar
+echo "Untarring imdb_crop.tar"
 tar -xf imdb_crop.tar
 rm imdb_crop.tar
 mkdir -p training_logs
